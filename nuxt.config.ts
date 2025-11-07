2266f6fe132288226188b9071e0528a3d78ceb26
Lam Hung Anh (Brim) <lamhunganh.vn@smilegate.com>
2025-11-07 14:37:30 +0700

    feat: design token

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,17 @@
 import tailwindcss from '@tailwindcss/vite';
 import { runtimeConfig } from './config/runtime';
+import { typescript } from './config/typescript';
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  typescript: {
-    tsConfig: {
-      include: [
-        '../config/**/*.ts', // relative path base on `.nuxt/tsconfig.app.json`
-      ],
-    },
-  },
+  css: ['~/assets/css/app.css'],
   vite: {
     plugins: [tailwindcss()],
   },
   imports: {
     dirs: ['constants'],
   },
+  typescript,
   runtimeConfig,
 });
