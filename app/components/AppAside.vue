95ac6c6395788c8ea7b6df78fe97f84c308dec5d
Lam Hung Anh (Brim) <lamhunganh.vn@smilegate.com>
2025-11-13 13:10:45 +0700

    feat: rnb

--- app/components/AppAside.vue
+++ app/components/AppAside.vue
@@ -1,3 +1,45 @@
 <template>
-  <aside>RNB</aside>
+  <aside>
+    <!-- title -->
+    <div class="leading-6 flex items-center justify-between">
+      <span class="text-md font-bold">불카누스 가이드</span>
+      <button class="p-2 size-8 bg-[#eff0f6] hover:bg-abd-active rounded-full relative">
+        <Icon name="svg:close-arrow-right" class="w-3 absolute -translate-1/2" />
+      </button>
+    </div>
+    <!-- toc -->
+    <ul class="leading-5.5 mt-1.5 text-sm text-tcl-dimmed">
+      <li><a href="#" class="text-primary font-medium">불카누스 소개</a></li>
+      <li class="mt-2 font-medium">
+        <a href="#">게임 프레임워크와 게임 템플릿</a>
+      </li>
+      <li class="mt-2 font-medium">
+        <a href="#">게임 프레임워크에서 제공되는 것</a>
+      </li>
+      <li class="mt-2 font-medium">
+        <a href="#">게임프레임워크의 구조</a>
+      </li>
+
+      <ul class="font-normal mt-1 pl-1">
+        <li class="border-l-1 border-l-abd-base py-1 hover:border-l-primary">
+          <a class="pl-4" href="#">파티 게임 프레임워크</a>
+        </li>
+        <li class="border-l-1 border-l-abd-base py-1 hover:border-l-primary">
+          <a class="pl-4" href="#">게임 템플릿</a>
+        </li>
+        <li class="border-l-1 border-l-abd-base py-1 hover:border-l-primary">
+          <a class="pl-4" href="#">스테이지</a>
+        </li>
+        <li class="border-l-1 border-l-abd-base py-1 hover:border-l-primary">
+          <a class="pl-4" href="#">장치 시스템</a>
+        </li>
+        <li class="border-l-1 border-l-abd-base py-1 hover:border-l-primary">
+          <a class="pl-4" href="#">게임 테스트</a>
+        </li>
+        <li class="border-l-1 border-l-abd-base py-1 hover:border-l-primary">
+          <a class="pl-4" href="#">게임 출판</a>
+        </li>
+      </ul>
+    </ul>
+  </aside>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,13 +7,13 @@ definePageMeta({
 <template>
   <main class="relative mx-auto flex max-w-7xl">
     <AppLNB
-      class="sticky top-0 hidden h-screen w-68 shrink-0 overflow-y-auto p-6 pt-8 md:block"
+      class="sticky mr-26 top-0 hidden h-screen w-68 shrink-0 overflow-y-auto p-6 pt-8 md:block"
     />
 
     <AppArticle class="min-w-80 pt-10 pb-38.5" />
 
     <AppAside
-      class="sticky top-0 hidden h-screen w-72 shrink-0 overflow-y-auto p-8 lg:block"
+      class="sticky top-0 ml-10 hidden h-screen w-51 shrink-0 overflow-y-auto pt-8 lg:block"
     />
   </main>
 </template>
