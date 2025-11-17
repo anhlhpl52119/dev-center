643846e68c0dded30a82a2b357fcbc4c4d2a1eb0
Lam Hung Anh (Brim) <lamhunganh.vn@smilegate.com>
2025-11-17 14:06:25 +0700

    feat: default path

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,11 @@
 <script lang="ts" setup>
 definePageMeta({ name: 'home' });
 const localePath = useLocalePath();
-navigateTo(localePath('/web/etc/Generator'), { replace: true });
+const { locale } = useI18n();
+if (locale.value === 'ko') {
+  navigateTo(localePath('/web/etc/Generator'), { replace: true });
+}
+else {
+  navigateTo(localePath('/web/auth/gnb/GNB-디자인'), { replace: true });
+}
 </script>
