9afcb625447427cf68685335fe59fe0b2c20e7dc
Hung Anh <anhlhpl52119@gmail.com>
2025-12-10 00:04:42 +0700

    feat: common

--- app/utils/common.ts
+++ app/utils/common.ts
@@ -1,3 +1,17 @@
 export function slugify(s: string) {
   return encodeURIComponent(String(s).trim().toLowerCase().replace(/\s+/g, '-'));
 }
+
+export function isNull(value: any): value is null {
+  return value === null;
+}
+
+/** check if value is undefined */
+export function isUndefined(value: any): value is undefined {
+  return value === undefined;
+}
+
+/** null or undefined */
+export function isNullish(value: any): value is null | undefined {
+  return isNull(value) || isUndefined(value);
+}
