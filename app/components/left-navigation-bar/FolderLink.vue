6b66a63c378b9c28bbe65c0fbc0abe994315d2d2
Hung Anh <anhlhpl52119@gmail.com>
2025-12-12 10:41:10 +0700

    feat: transition

--- app/components/left-navigation-bar/FolderLink.vue
+++ app/components/left-navigation-bar/FolderLink.vue
@@ -26,53 +26,51 @@ function toggleExpand(id: number) {
 </script>
 
 <template>
-  <NavLinkItem :item="item">
+  <NuxtLinkLocale
+    :to="`/${item.path}`"
+    exactActiveClass="!bg-abg-active text-primary"
+    class="focus-visible:ring-primary text-15 flex w-full items-center gap-8 py-8 pr-12 pl-16 font-medium rounded-full capitalize transition-all duration-200 hover:bg-gray-500/5 focus-visible:ring-1 focus-visible:outline-none"
+  >
     <span class="flex-1">
       {{ item.title }}
     </span>
 
-    <button
-      v-if="!!item.children?.length"
-      :aria-expanded="isExpanded"
-      tabindex="-1"
-      :aria-label="`${isExpanded ? 'Collapse' : 'Expand'} ${item.title}`"
-      class="hover:text-primary size-16 cursor-pointer rounded-full transition-all duration-200 hover:bg-gray-500/20"
-      @click.prevent="toggleExpand(item.id)"
-    >
-      <Icon
-        name="svg:single-arrow-down"
-        class="h-6 w-10 align-[0.34em] leading-24 transition-all duration-200"
-        :class="[
-          isExpanded ? '-rotate-180 align-[-0.4em]' : '',
-        ]"
-        aria-hidden="true"
-      />
-    </button>
-  </NavLinkItem>
-
-  <Transition
-    v-if="!!item.children?.length"
-    enterActiveClass="transition-all duration-300 ease-[cubic-bezier(0.4,0,0.2,1)]"
-    leaveActiveClass="transition-all duration-250 ease-[cubic-bezier(0.4,0,0.6,1)]"
-    enterFromClass="opacity-0 max-h-0 scale-y-0 origin-top"
-    enterToClass="opacity-100 max-h-1000 scale-y-100 origin-top"
-    leaveFromClass="opacity-100 max-h-1000 scale-y-100 origin-top"
-    leaveToClass="opacity-0 max-h-0 scale-y-0 origin-top"
-  >
-    <ul
-      v-if="isExpanded"
-      class="pl-16 space-y-2"
-    >
-      <li
-        v-for="navItem in item.children"
-        :key="navItem.id"
+    <!-- folder expand icon -->
+    <template v-if="!!item.children?.length && item.isFolder">
+      <button
+        :aria-expanded="isExpanded"
+        tabindex="-1"
+        :aria-label="`${isExpanded ? 'Collapse' : 'Expand'} ${item.title}`"
+        class="hover:text-primary size-16 cursor-pointer rounded-full transition-all duration-200 hover:bg-gray-500/20"
+        @click.prevent="toggleExpand(item.id)"
       >
-        <FolderLink
-          :expandIds="expandIds"
-          :item="navItem"
-          :isRoot="false"
+        <Icon
+          name="svg:single-arrow-down"
+          class="h-6 w-10 align-[0.34em] leading-24 transition-all duration-200"
+          :class="[
+            isExpanded ? '-rotate-180 align-[-0.4em]' : '',
+          ]"
+          aria-hidden="true"
         />
-      </li>
-    </ul>
+      </button>
+    </template>
+  </NuxtLinkLocale>
+
+  <Transition name="fold">
+    <!-- folder -->
+    <template v-if="item.children?.length && isExpanded">
+      <ul class="pl-16 mt-2 space-y-2">
+        <li
+          v-for="navItem in item.children"
+          :key="navItem.id"
+        >
+          <FolderLink
+            :expandIds="expandIds"
+            :item="navItem"
+            :isRoot="false"
+          />
+        </li>
+      </ul>
+    </template>
   </Transition>
 </template>
